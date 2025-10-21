f4fa2bc281e929106e13ab3060876527d083ba65
Kepo Amat Dah <rebelscodeee@gmail.com>
2025-10-21 21:20:41 +0700

    Update checkpoint coordinates for Mount Kawaii

--- AutoSummit.lua
+++ AutoSummit.lua
@@ -1,27 +1,38 @@
 -- 🌋 AUTO SUMMIT COLLECTION BY [USERNAME]
 -- Kamu bisa tambah gunung baru cukup tambahkan satu blok baru di bawah.
 --[Fungsi untuk menambah gunung
-{
-    Name = "Mount Salvatore",
-    Delay = 0.4,
-    Checkpoints = {
-        CFrame.new(150, 100, 200),
-        CFrame.new(500, 300, 150),
-        CFrame.new(800, 450, 50),
-    }
-}
-]--
+--{
+    --Name = "Mount Salvatore",
+    --Delay = 0.4,
+    --Checkpoints = {
+        --CFrame.new(150, 100, 200),
+        --CFrame.new(500, 300, 150),
+        --CFrame.new(800, 450, 50),
+   -- }
+-- }
+
 
 return {
     {
         Name = "Mount Kawaii",
         Delay = 0.3,
         Checkpoints = {
-            CFrame.new(120, 85, 310),
-            CFrame.new(400, 150, 200),
-            CFrame.new(750, 280, 100),
-            CFrame.new(980, 400, 50),
-            CFrame.new(1250, 550, 0),
+            CFrame.new(277, 82.7190018, 249),
+            CFrame.new(584, 273.859009, -57),
+            CFrame.new(1201, 271.320007, 294),
+            CFrame.new(1553, 285.02301, 289),
+            CFrame.new(1819.94104, 324.519989, -9.05200005),
+            CFrame.new(2260, 395.670013, 110),
+            CFrame.new(3168, 511, 335),
+            CFrame.new(3672, 492.184998, 251.675003),
+            CFrame.new(4221, 490.42099, 516),
+            CFrame.new(4414.35693, 706.960022, 809.830017),
+            CFrame.new(4370, 700.062012, 1876),
+            CFrame.new(5029, 443.921997, 2445),
+            CFrame.new(5225.271, 445.399994, 3078.11011),
+            CFrame.new(5253.43994, 445.166992, 3866.80005),
+            CFrame.new(5314.5, 692.393982, 4057.86011),
+            CFrame.new(5431, 1094.21997, 4402)
         }
     },
 
