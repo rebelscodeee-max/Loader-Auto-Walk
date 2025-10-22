f6d217d860cc37ba8f2588a2b8b1818b2ad8d873
Kepo Amat Dah <rebelscodeee@gmail.com>
2025-10-22 09:13:00 +0700

    Add files via upload

---
+++ AutoSummit.lua
@@ -0,0 +1,18 @@
+-- SummitData.lua
+return {
+    ["Mount Yahayuk"] = {
+        CFrame.new(277, 82.7, 249),
+        CFrame.new(584, 273.8, -57),
+        CFrame.new(1201, 271.3, 294),
+    },
+    ["Mount Pargoy"] = {
+        CFrame.new(225, 90, 320),
+        CFrame.new(890, 200, 180),
+        CFrame.new(1430, 450, 350),
+    },
+    ["Mount Ciremai"] = {
+        CFrame.new(-300, 100, 500),
+        CFrame.new(-900, 400, 750),
+        CFrame.new(-1400, 900, 1000),
+    }
+}
