7a2cb1959f5d4b44af46671a96fe403ef691d0d8
Kepo Amat Dah <rebelscodeee@gmail.com>
2025-10-31 23:13:57 +0700

    Update Music Player.lua

--- Music Player.lua
+++ Music Player.lua
@@ -185,6 +185,7 @@ local songs = {
 	{ Name="He Knows My Name", Id="112156011935123" },
 	{ Name="Salah Dola-dola", Id="139590201617508" },
 	{ Name="TANPA JUDUL", Id="111417042398302" },
+	{ Name="VOW", Id="73582098001514" },
 	{ Name="DJ Sayang Culik aku dong", Id="119254319180287" }
 }
 local sound = Instance.new("Sound", workspace)
